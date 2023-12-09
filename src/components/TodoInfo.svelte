93382c86cc91a35e56118a68227e185cf9f359af
Gigiox <guillermo.casanova.b@gmail.com>
2023-12-09 17:53:54 -0300

    feat: create todo info component

---
+++ src/components/TodoInfo.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { deleteTodo, toggleCompletedTodo } from '../stores/todoStore';
+	import type { Todo } from '../interfaces/Todo.interfaces';
+	export let todo: Todo;
+	export let index: number;
+</script>
+
+<li class="bg-white flex items-center shadow-sm border border-gray-200 rounded-lg my-2 py-2 px-4">
+	<input
+		name="completed"
+		type="checkbox"
+		checked={todo.completed}
+		on:change={() => toggleCompletedTodo(todo.id)}
+		class="mr-2 form-checkbox h-5 w-5"
+	/>
+
+	<span class={`flex-1 text-gray-800 ${todo.completed ? 'line-through' : ''}`}>{todo.text}</span>
+
+	<button
+		class="text-sm bg-red-500 hover:bg-red-600 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+		type="button"
+		on:click={() => deleteTodo(todo.id)}>Delete</button
+	>
+</li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,14 @@
-<script>
+<script lang="ts">
+	import TodoInfo from '../components/TodoInfo.svelte';
 	import TodoForm from '../components/TodoForm.svelte';
+	import { todos } from '../stores/todoStore';
 </script>
 
 <main>
 	<h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">My Todos</h1>
 	<TodoForm />
+
+	{#each $todos as todo}
+		<TodoInfo {todo} index={todo.id} />
+	{/each}
 </main>
